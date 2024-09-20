e236aa65f9bf3bb777a22a22004cf8c56e11c9f1
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-09-20 15:38:46 -0300

    Search js refactor

---
+++ assets/js/_firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBMaH9kpG7YeadV53rSX_iHYVdzyVOXJK8",
+  authDomain: "login-2b477.firebaseapp.com",
+  projectId: "login-2b477",
+  storageBucket: "login-2b477.appspot.com",
+  messagingSenderId: "809242408627",
+  appId: "1:809242408627:web:5219ad64cb35cd0584e2c6",
+};
+firebase.initializeApp(firebaseConfig);
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,23 +1,15 @@
 /* Search */
 const searchInput = document.getElementById("searchInput");
-
-// Adiciona um event listener ao campo de busca
 searchInput.addEventListener("input", filterList);
 
 function filterList() {
   const searchTerm = searchInput.value.toLowerCase();
-
-  // Filtra as linhas da tabela
   const rows = document.querySelectorAll("#tbody tr");
   rows.forEach((row) => {
     const linkName = row.querySelector("a").textContent.toLowerCase();
     const tag = row.querySelector(".tag").textContent.toLowerCase();
-
-    // Verifica se o nome ou tag contém o termo de busca
     const matchesSearch =
       linkName.includes(searchTerm) || tag.includes(searchTerm);
-
-    // Mostra ou esconde a linha da tabela
     row.style.display = matchesSearch ? "" : "none";
   });
 }
@@ -25,7 +17,6 @@ function filterList() {
 /* SERACH POR TAG */
 const tagOption = [];
 
-/* Atualiza as opções de tag no <select> */
 function updateTagOptions() {
   const select = document.getElementById("tagSearch");
   select.innerHTML = `<option value=>All TAG's</option>`;
@@ -43,13 +34,10 @@ document.getElementById("tagSearch").addEventListener("change", filterByTag);
 
 function filterByTag() {
   const selectedTag = document.getElementById("tagSearch").value.toLowerCase();
-
   const rows = document.querySelectorAll("#tbody tr");
   rows.forEach((row) => {
     const rowTag = row.querySelector(".tag").textContent.toLowerCase();
-
     const matchesTag = selectedTag === "" || rowTag === selectedTag;
-
     row.style.display = matchesTag ? "" : "none";
   });
 }
